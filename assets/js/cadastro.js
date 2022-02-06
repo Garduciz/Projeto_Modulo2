ded90afd7e3f9cad0d817830bfa060098bc569f3
MatricBts <matricbts@gmail.com>
2022-02-06 18:05:51 -0300

    iniciando array para salvar dados do cliente

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -6,11 +6,32 @@ const rg = $( '#rg' );
 const cep = $( '#cep' );
 const botao = $( '#btnCadastro' );
 const test = '41185210';
+var array = []
 
 botao.on( 'click', (event) => {
     event.preventDefault();
    
-          
+    // let clientes = [];
+
+    // class Cliente{
+    //     constructor( nome, email ){
+    //         this.nome = nome,
+    //         this.email = email
+    //     }
+    // }
+    
+    // let cliente = new Cliente( nome.val(), email.val() )
+    // clientes.push( cliente )
+
+    // console.log(clientes)
+
+    let i = 0;
+    let test = prompt()
+
+
+
+    array.push( test )
+    console.log( array )
 })
 
 cep.on( 'change', ()=>{
