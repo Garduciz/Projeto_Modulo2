5a352e84ff226c58dc6d6080e40c47214ec69d62
Matric Bastos <matricbts@gmail.com>
2022-02-11 17:47:29 -0300

    determinando condicionais do login, para ver o acesso do usuario

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,37 +6,35 @@ const dadosStorage = JSON.parse( localStorage.getItem( 'clientes' ) );
 botao.on( 'click', (event) => {
     event.preventDefault();
 
-    confereLogin()
+   confereCampos()
     
 })
 
 const confereLogin = () => {
-    
-    let dadosIncorretos = false ;
 
-    for (let i = 0; i <= email.length; i++) {
-        if( dadosStorage[i].email === email.val() && dadosStorage[i].senha === senha.val() ){
-            alert( 'Esse email já está cadastrado na nossa fila de espera' );
-            break;
+    let dadosConfere = false;
+
+    for ( let i = 0; i < dadosStorage.length; i++ ) {
+        if( email.val() === dadosStorage[i].email && senha.val() === dadosStorage[i].senha ){
+            dadosConfere = true;
         }
-        else if( dadosStorage[i].email === email.val() && dadosStorage[i].senha !== senha.val() ){
-            dadosIncorretos = true;
-            break;
-        }
-        else{
-           dadosIncorretos = false;
-        }   
     }
 
-    if( dadosIncorretos){
-        alert( 'Dados incorretos' );
+    if( dadosConfere ){
+        alert( 'VOCE JÁ ESTA CADASTRADO, LOGO TERA ACESSO LIBERADO' );
     }
     else{
-        alert( 'VocÊ ainda não foi cadastrado' );
-        window.location.href = "cadastro.html";
+        alert( 'OS DADOS NÃO CONFEREM, VEJA SUA SENHA OU EMAIL' );
     }
 }
 
 
-
-
+const confereCampos = () => {
+    if( email.val() === '' || senha.val() === '' ){
+        alert( 'PREENCHA TODOS OS CAMPOS' );
+    }
+    else{
+        confereLogin()
+    }
+    
+}
