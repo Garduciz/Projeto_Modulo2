8f7ead5522a3e6e2bd709218c4fa748f63fff2b4
Matric Bastos <matricbts@gmail.com>
2022-02-14 13:13:05 -0300

    melhorando descrição do codigo

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -11,85 +11,100 @@ const cidade = $( '#cidade' );
 const uf = $( '#uf' );
 const complemento = $( '#complemento' );
 
-const testando = JSON.parse(localStorage.getItem( 'clientes' ) )
-console.log( testando[testando.length-1].nome )
-
 const botao = $( '#btnCadastro' );
 const inputs = $( 'input' );
 
-// EVENTOS 
-botao.on( 'click', ( event )=> {
+
+
+
+// EVENTOS FEITOS DURANDO O CADASTRO 
+//====================================================================================================
+botao.on( 'click', ( event )=> { // QUANDO O BOTAO CADASTRAR FOR APERTADO ELE EFETUADO AS FUNÇOES PARA CADASTRO
     event.preventDefault();
-    conferindoDados();
+    confereEmailCadastrado( clientesStorage() );
 })
 
-cep.on( 'change', buscarCep );
+// PREENCHENDO O CAMPO CEP
+cep.on( 'change', buscarCep ); // SEMPRE QUE O CAMPO CEP FOR MUDADADO ELE EXECUTA A FUNÇÃO DE BUSCARCEP
+//====================================================================================================
 
-//-------------------------------------------------------------------------------------------------
+
+// PEGANDO DADOS DO CEP COM A API
+//=====================================================================================================
 // BUSCANDO O CEP
 function buscarCep(){
-    const busca = cep.val();
-    $.getJSON(`https://viacep.com.br/ws/${busca}/json/`, ( endereco ) => {
-        console.log( endereco )
-
+    const busca = cep.val();// BUSCA É O VALOR DO CEP DIGITADO QUE A GENTE COLOCA NA REQUISIÇÃO A API
+    $.getJSON(`https://viacep.com.br/ws/${busca}/json/`, ( endereco ) => { // FAZ UMA REQUISIÇÃO A API,
+    // A REQUISIÇÃO TRAS UM OBJETO, QUE A GENTE UTILIZA NO PARAMETRO ENDERECO.
         rua.val( endereco.logradouro );
         bairro.val( endereco.bairro );
         cidade.val( endereco.localidade );
         uf.val( endereco.uf );
     })
 }
+//=======================================================================================================
 
 
-//--------------------------------------------------------------------------------------------------
+// CRIANDO OBJETOS
+//=======================================================================================================
 //CRIANDO OBJETO ENDEREÇO
 const criaEndereco = ( ) => {
     const endereco = new Endereco( cep.val(), rua.val(), numero.val(), bairro.val(), cidade.val(), uf.val(), complemento.val() );
     return endereco;
 }
 
-
-//----------------------------------------------------------------------------------------------------
 // CRIANDO OBJETOS CLIENTE
 const criaCliente = ( enderecoObjeto, planoEscolhido ) =>{
     const cliente = new Cliente( nome.val(), email.val(), senha.val(), rg.val(), enderecoObjeto, planoEscolhido ); 
     return cliente;
 }
+//==========================================================================================================
 
 
-//--------------------------------------------------------------------------------------------------
-// SALVANDO DADOS
-const salvaStorage = ( cliente ) => {
-    localStorage.setItem( 'clientes', JSON.stringify( cliente ) ) ;
-    // ENVIA PARA NOSSO STORAGE DE NOME CLIENTES DADOS DO OBJETO CLIENTE                                                           
-}
-
-
-//---------------------------------------------------------------------------------------------------
-//PEGANDO DADOS
+// MANIPULANDO O STORAGE
+//=========================================================================================================
+// PROCESSO PARA PEGAR DADOS DO STORAGE
 const buscaStorage = () =>{
     // BUSCA NO STORAGE, E CASO NÃO TENHA NADA CRIADO ELE RETORNA UM ARRAY VAZIO
     if ( !localStorage.getItem( 'clientes' ) ){
-        return [];
+        return []; // NOSSO ARRAY VAZIO
     }
     else{ // SE TIVER ALGO ELE RETORNA AS INFORMAÇÕES QUE ESTÃO LÁ 
-        return JSON.parse(localStorage.getItem( 'clientes' ) );
+        return JSON.parse( localStorage.getItem( 'clientes' ) );
+        // NO STORAGE É SALVO COMO STRING, A GENTE TRAZ COMO ARRAY DE OBJETO PARA A GENTE PODER TRABALHAR
+        // POR ISSO O JSON.PARSE
     }     
 } 
 
 
-//----------------------------------------------------------------------------------------------------
-// COLOCANDO CLIENTE NO STORAGE
-const salvaCliente = ( cliente ) => {
-    const clientes = buscaStorage(); // ELE PEGA AS INFORMAÇOES QUE ESTÃO NO STORAGE
-    clientes.push ( cliente ); // ELE ADICIONA AS INFORMÇÕES DO NOVO CLIENTE
-    salvaStorage( clientes ); // SALVA AS NOVAS INFORMAÇÕES NO STORAGE CLIENTES
-    alert( ` Parabéns ${ clientes[clientes.length-1].nome }, seu cadastro foi efetuado com sucesso com o plano: Focus ${ clientes[clientes.length-1].plano }` );
-    window.location.href = 'sobre.html'
+// COLOCA DADOS NO STORAGE
+const salvaStorage = ( cliente ) => {
+    localStorage.setItem( 'clientes', JSON.stringify( cliente ) ) ;
+    // ENVIA PARA NOSSO STORAGE DE NOME CLIENTES DADOS DO OBJETO CLIENTE 
+    // A GENTE PASSA O OBJETO CLIENTE COMO STRING, POR ISSO O STRINGIFY                                                          
 }
 
 
-//----------------------------------------------------------------------------------------------------
-//CONFERE A ESCOLHA DO PLANO
+// CLIENTES DO STORAGE
+const clientesStorage = () => {
+    const clientes = buscaStorage(); // UTILIZAMOS A FUNCAO PARA PEGAR OS DADOS NO STORAGE E SALVAMOS EM CLIENTES
+    return clientes; // RETORNAMOS CLIENTES
+}
+
+
+// COLOCANDO CLIENTE NO STORAGE
+const salvaCliente = ( cliente, arrayClientes ) => { // PEGA COMO PARAMETRO O CLIENTE NOVO E O ARRAY COM OS CLIENTES CADASTRADOS
+    arrayClientes.push ( cliente ); // ELE ADICIONA AS INFORMÇÕES DO NOVO CLIENTE NO ARRAY DE CLIENTES EXISTENTES
+    salvaStorage( arrayClientes ); // ELE MANDA O ARRAY DE CLIENTES SALVOS PARA O STORAGE
+
+    alert( ` Parabéns ${ arrayClientes[ arrayClientes.length-1].nome }, seu cadastro foi efetuado com sucesso com o plano: ${ arrayClientes[ arrayClientes.length-1 ].plano }` );
+    window.location.href = 'sobre.html'
+}
+//=========================================================================================================
+
+
+// ESCOLHA DO PLANO
+//=========================================================================================================
 const conferindoPlano = () =>{
     for (let i = 0; i < inputs.length; i++) { // ELE BUSCA EM TODOS OS INPUS
         if( inputs[i].checked === true){ // SE O INPUT TIVER TICADO (SOMENTE OS DOS PLANOS PODEM SER TICADOS)
@@ -97,34 +112,59 @@ const conferindoPlano = () =>{
         }    
     }
 }
+//=========================================================================================================
 
 
-//----------------------------------------------------------------------------------------------------
-//CONFERE TODOS OS CAMPOS
+//CONFERINDO TODOS OS CAMPOS
+//========================================================================================================
 const conferindoDados = () =>{
-    if( !nome.val() || !email.val() || !senha.val() || !rg.val() || !cep.val() ){
+    if( !nome.val() || !email.val() || !senha.val() || !rg.val() || !cep.val() ){// CONFERE SE TODOS OS CAMPOS ESTÃO PREENCHIDOS
         alert( 'PREENCHA TODOS OS CAMPOS' );
     }
-    else if( senha.val() !== senhaConfirmacao.val() ){
+    else if( senha.val() !== senhaConfirmacao.val() ){ // CONFERE SE AS SENHAS SÃO IGUAIS
         alert( 'A SENHAS ESTÃO DIFERENTE' );
     }
-    else if ( rg.val().length !== 10 ){
+    else if ( rg.val().length !== 10 ){ // CONFERE SE RG É MAIOR QUE 10
         alert( 'TAMANHO DO RG É INVALIDO')
     }
-    else if( !email.val().includes( '@' && '.com' ) ){
+    else if( !email.val().includes( '@' && '.com' ) ){ // CONFERE SE EMAIL POSSUI @ E .COM
         alert( 'INSIRA UM EMAIL VÁLIDO' );
     }
-    else{
-        salvaCliente( criaCliente( criaEndereco(), conferindoPlano() ) );
+    else{ // SE NÃO TIVER NENHUM PROBLEMA COM AS CONDIÇÕES A CIMA ELE EXECUTA O PROCESSO DE CADASTRO
+        salvaCliente( criaCliente( criaEndereco(), conferindoPlano() ), clientesStorage() );
     }
 }
-
-//----------------------------------------------------------------------------------------------------
-// LIMPANDO DADOS 
+//======================================================================================================
 
 
-//-------------------------------------------------------------------------------------------------------
-// Classes
+// CONFERINDO SE O EMAIL JÁ ESTA CADASTRADO NO STORAGE
+//======================================================================================================
+const confereEmailCadastrado = (clientes) => {
+    let emailExistente = false; // BOLENO QUE A GENTE VAI USAR PARA VERIFICAR SE O EMAIL JÁ ESTÁ CADASTRADO
+    
+    if( clientes.length >= 1){ // SE O ARRAY CLIENTES TIVER ALGUM ELEMENTO ELE EXECUTA O CODIGO ABAIXO
+        for (let i = 0; i < clientes.length ; i++) { // FOR PARA PASSAR POR TODOS OS ELEMENTOS DO ARRAY
+            if ( clientes[i].email === email.val() ) { //ELE FAZ UMA COMPARAÇÃO DOS EMAILS DOS ELEMENTOS, COM O EMAIL DIGITADO NO INPUT EMAIL
+                emailExistente = true; // EMAILEXISTENTE PASSA A SER VERDADEIRO
+                break
+            }else{
+                emailExistente = false; // EMAIL EXISTENTE CONTINUA SENDO FALSO
+            }
+        }
+    }
+    
+    //CONDICIONAL BASEADO NO EMAILEXISTENTE, O BOLEANO
+    if ( emailExistente ) {// SE EMAIL EXISTENTE FOR VERDADEIRO, ELE AVISA A O USUARIO
+        alert( 'Email já cadastrado' );
+    } else {
+        conferindoDados(); // SE FOR FALSO ELE EXECUTA O PROCESSO DE CADASTRO
+    }
+}
+//=====================================================================================================
+
+
+// CRIANDO CLASSES
+//=====================================================================================================
 class Cliente{
     constructor( nome, email, senha, rg, endereco, plano ){
         this.nome = nome,
@@ -147,5 +187,4 @@ class Endereco{
         this.complemento = complemento
     }
 }
-
-
+//======================================================================================================
