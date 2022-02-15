946aa6dfed612f25ecce0f77817348f886d40f12
Matric Bastos <matricbts@gmail.com>
2022-02-14 23:49:01 -0300

    finalizando login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,25 +1,26 @@
 const email = $( '#email' );
 const senha = $( '#senha');
-const botao = $( '#btn' );
+const botao = $( '#btnLogin' );
 const dadosStorage = JSON.parse( localStorage.getItem( 'clientes' ) );
 
+//EVENTO USUARIO
+//===================================================================================================
 botao.on( 'click', (event) => {
     event.preventDefault();
-
    confereCampos()
-    
 })
+//==================================================================================================
 
+
+//CONFERINDO CREDENCIAIS
+//===================================================================================================
 const confereLogin = () => {
-
     let dadosConfere = false;
-
     for ( let i = 0; i < dadosStorage.length; i++ ) {
         if( email.val() === dadosStorage[i].email && senha.val() === dadosStorage[i].senha ){
             dadosConfere = true;
         }
     }
-
     if( dadosConfere ){
         alert( 'VOCE JÁ ESTA CADASTRADO, LOGO TERA ACESSO LIBERADO' );
     }
@@ -27,14 +28,17 @@ const confereLogin = () => {
         alert( 'OS DADOS NÃO CONFEREM, VEJA SUA SENHA OU EMAIL' );
     }
 }
+//====================================================================================================
 
 
+// CONFERE SE OS CAMPOS ESTÃO PREENCHIDOS
+//=====================================================================================================
 const confereCampos = () => {
     if( email.val() === '' || senha.val() === '' ){
         alert( 'PREENCHA TODOS OS CAMPOS' );
     }
     else{
         confereLogin();
-    }
-    
+    }    
 }
+//======================================================================================================
